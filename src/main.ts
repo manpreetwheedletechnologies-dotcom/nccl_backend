9f0ac2c22055662f10b4c11da6b444462965ce27
Mac <mac@Macs-MacBook-Pro.local>
2026-02-21 13:18:28 +0530

    Update main.ts and sync latest backend improvements

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     app.enableCors();
     const port = process.env.PORT || 8000;
     await app.listen(port, '0.0.0.0');
-    console.log(`Application is running on: http://0.0.0.0:${port}`);
+    console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
